26441de4e05c72de8140371d6bb254b89f0cd063
SeokWoong Han <hseokwoo@amazon.com>
2021-11-08 00:49:02 +0900

    last2

--- scripts/mkdir.sh
+++ scripts/mkdir.sh
@@ -1,5 +1,5 @@
 sudo mkdir -p /web
 curl -O https://bootstrap.pypa.io/get-pip.py
 python3.7 get-pip.py --user
-python3.7 -m pip install Flask
+python3.7 -m pip install Flask requests
 sudo chmod 755 /web/scripts/*.sh
--- scripts/stop_flask1.sh
+++ scripts/stop_flask1.sh
@@ -1,1 +1,1 @@
-sudo python3.7 /web/scripts/stop_flask.py
+python3.7 /web/scripts/stop_flask.py
