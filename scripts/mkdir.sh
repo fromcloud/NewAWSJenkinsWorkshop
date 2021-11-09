cde4b9515eda42373f3a4dbdb4f1f15cb68e5b28
SeokWoong Han <hseokwoo@amazon.com>
2021-11-09 14:03:16 +0900

    last20

--- scripts/mkdir.sh
+++ scripts/mkdir.sh
@@ -1,5 +1,9 @@
 sudo mkdir -p /web
 curl -O https://bootstrap.pypa.io/get-pip.py
 python3.7 get-pip.py --user
-python3.7 -m pip install Flask requests aws-xray-sdk
+curl https://s3.dualstack.ap-northeast-2.amazonaws.com/aws-xray-assets.ap-northeast-2/xray-daemon/aws-xray-daemon-3.x.rpm -o /home/ec2-user/xray.rpm
+yum install -y /home/ec2-user/xray.rpm
+pip3 install Flask
+pip3 install requests
+pip3 install aws-xray-sdk
 sudo chmod 755 /web/scripts/*.sh
